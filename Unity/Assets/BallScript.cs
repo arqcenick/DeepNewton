ff75b3468d156d488d360ee54bd890b677a7a8bf
YarkinDCetin <yarkindc@gmail.com>
2017-04-21 11:40:48 +0300

    Unity3D Codes

---
+++ Unity/Assets/BallScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+
+    Rigidbody rigid;
+    float time = 30f;
+    Vector2 vec;
+
+
+
+    float power;
+    private void Awake()
+    {
+         vec = Random.insideUnitCircle;
+        power = 20000f + Random.value * 20000f;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        
+        rigid = gameObject.GetComponent<Rigidbody>();
+        rigid.AddForce(new Vector3(vec.x * power, 0f, vec.y * power));
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        }
+	}
+}
---
+++ Unity/Assets/Capture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capture : MonoBehaviour {
+    public string folder = "ScreenshotFolder2";
+    public int frameRate = 25;
+
+
+    // Use this for initialization
+    void Start () {
+        Time.captureFramerate = 8;
+
+        // Create the folder
+        System.IO.Directory.CreateDirectory(folder);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        string name = string.Format("{0}/{1:D04}_shot.png", folder, Time.frameCount);
+        Application.CaptureScreenshot(name);
+    }
+}
